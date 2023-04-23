d429f6128f1e035fe40a3b7eccc22d9509b38232
wagnernazarios <wagnernazario124@gmail.com>
2023-04-23 19:25:15 -0300

    add new picture with js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function toggleMode(){
   const html = document.documentElement;
-    html.classList.toggle("lighty")
+
+  //trocar entre o modo dark e light
+  html.classList.toggle("lighty")
+
+
+  // troca imagem
+   const img = document.querySelector("#profile img")
+
+   if (html.classList.contains("lighty")) {
+    img.setAttribute('src', 'assets/Avatar-light.png');
+
+    img.setAttribute('alt', 'foto de profile light mode')
+   } else {
+img.setAttribute("src", "assets/Avatar.png")
+  }
 };
